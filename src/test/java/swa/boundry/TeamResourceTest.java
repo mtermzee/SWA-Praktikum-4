284ec7dabc8267c0b61db252eac4d496ba2c03cd
mtermzee <mohammad.termzee@hs-osnabrueck.de>
2022-11-30 12:39:02 +0100

    add test

--- src/test/java/swa/boundry/TeamResourceTest.java
+++ src/test/java/swa/boundry/TeamResourceTest.java
@@ -1,11 +1,14 @@
 package swa.boundry;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import static org.hamcrest.CoreMatchers.*;
+import static io.restassured.RestAssured.given;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -18,47 +21,53 @@ public class TeamResourceTest {
 
     @BeforeAll
     public void init() {
-
-    }
-
-    @Test
-    void testAddTeamMember() {
-
-    }
-
-    @Test
-    void testConvertToDTO() {
-
-    }
-
-    @Test
-    void testConvertToDTO2() {
-
+        teamResource.createTeam("Barcelona", "Team");
     }
 
     @Test
     void testCreateTeam() {
-
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().post("/Teams/Real Madrid?category=Team}")
+                .then()
+                .statusCode(200)
+                .body(is("Team has been added to Team successfully"));
     }
 
     @Test
     void testDeleteTeam() {
-
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().delete("/Teams/0")
+                .then()
+                .statusCode(200)
+                .body(is("Team has been removed successfully"));
     }
 
     @Test
-    void testDeleteTeamMember() {
-
+    void testUpdateTeam() {
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().put("/Teams/0?name=Real Madrid&category=Team")
+                .then()
+                .statusCode(200)
+                .body(is("Team has been updated successfully"));
     }
 
     @Test
     void testGetTeamById() {
-
+        given()
+                .when().get("/Teams/{id}", 0)
+                .then()
+                .statusCode(200);
     }
 
     @Test
     void testGetTeams() {
-
+        given()
+                .when().get("/Teams")
+                .then()
+                .statusCode(200);
     }
 
 }
